fa2b54ad3d3502c55c2d5fed4bf6d24fb7575417
Neil Thistlethwaite <11429656+nthistle@users.noreply.github.com>
2019-01-10 05:19:41 -0500

    Add hunt-and-kill logic to Crusaders

--- sprintbot/CrusaderHandler.java
+++ sprintbot/CrusaderHandler.java
@@ -6,12 +6,43 @@ public class CrusaderHandler extends RobotHandler
         super(robot);
     }
 
+    boolean mapSymmetry;
+    Direction[][] presumedMap;
+
     public void setup() {
         robot.log("Crusader setup called!");
     }
 
     public Action turn() {
-        robot.log("Crusader turn called!");
+        // first look for closest enemy
+        Coordinate myLoc = Coordinate.fromRobot(robot.me);
+
+        Robot nearestEnemy = null;
+        int nearestDist = -1;
+
+        for (Robot r : robot.getVisibleRobots()) {
+            if (r.team != robot.me.team) {
+                if (nearestEnemy == null || Utils.getDistance(myLoc, Coordinate.fromRobot(r)) < nearestDist) {
+                    nearestEnemy = r;
+                    nearestDist = Utils.getDistance(myLoc, Coordinate.fromRobot(r));
+                }
+            }
+        }
+
+        if (nearestEnemy != null) {
+
+            if (nearestDist <= 16) {
+                // attack range, blast them
+                return robot.attack(nearestEnemy.x - robot.me.x, nearestEnemy.y - robot.me.y);
+            }
+
+            // path towards them, could be slightly time-intensive, but 64^2 is pretty trivial
+            Direction[][] targetMap = Utils.getDirectionMap(robot.map, nearestEnemy.x, nearestEnemy.y);
+
+            Direction netDir = Utils.followDirectionMap(targetMap, robot.getVisibleRobotMap(), 9, robot.me.x, robot.me.y);
+            return robot.move(netDir.dx, netDir.dy);
+        }
+
 
         Direction d = Utils.getRandomDirection();
         return robot.move(d.dx, d.dy);
